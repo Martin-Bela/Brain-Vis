75ec9f67fc2318b658250c0a615bf9b6b936e3ef
Martin Beľa <xbela1@fi.muni.cz>
2023-03-01 23:39:32 +0100

    Add Vtk hello word

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
-#include <format>
+#include <vtkAutoInit.h>
 
+VTK_MODULE_INIT(vtkRenderingOpenGL2);
+VTK_MODULE_INIT(vtkRenderingFreeType);
+VTK_MODULE_INIT(vtkInteractionStyle);
+
+#include <vtkSmartPointer.h>
+#include <vtkTextActor.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
 
 int main() {
-    std::cout << std::format("Hello {}", "World!");
+    auto textActor = vtkSmartPointer<vtkTextActor>::New();
+    textActor->SetInput("Hello World");
+
+    auto renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor(textActor);
+    renderer->ResetCamera();
+
+    auto interactor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+
+    auto renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetInteractor(interactor);
+
+    interactor->Start();
+
     return 0;
 }
