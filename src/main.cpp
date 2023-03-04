b02e8bf844015259d178705aca25dc7b39c0f2ad
Martin Beľa <xbela1@fi.muni.cz>
2023-03-04 14:29:27 +0100

    Add drawing points with color

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <vtkNamedColors.h>
 #include <vtkNew.h>
 #include <vtkPolyDataMapper.h>
+#include <vtkPointData.h>
 #include <vtkProperty.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
@@ -24,7 +25,7 @@ namespace { //anonymous namespace
 
     std::filesystem::path dataFolder = "../data/viz-calcium";
 
-    vtkNew<vtkNamedColors> colors;
+    vtkNew<vtkNamedColors> namedColors;
 
     vtkNew<vtkPoints> loadPositions() {
         vtkNew<vtkDelimitedTextReader> reader;
@@ -34,23 +35,58 @@ namespace { //anonymous namespace
         reader->SetFieldDelimiterCharacters(" ");
         reader->Update();
 
-        vtkNew<vtkPoints> result;
+        vtkNew<vtkPoints> positions;
         vtkTable* table = reader->GetOutput();
         for (vtkIdType i = 0; i < table->GetNumberOfRows(); i++)
         {
-            result->InsertNextPoint(
+            positions->InsertNextPoint(
                 (table->GetValue(i, 1)).ToDouble(),
                 (table->GetValue(i, 2)).ToDouble(),
                 (table->GetValue(i, 3)).ToDouble());
         }
-        return result;
+        return positions;
+    }
+
+    vtkNew<vtkUnsignedCharArray> loadColors(uint64_t timestep, uint64_t pointCount) {
+        /*vtkNew<vtkDelimitedTextReader> reader;
+        auto path = (dataFolder / "monitors/0_").string();
+        path += std::to_string(timestep) + ".csv";
+        
+        reader->SetFileName(path.data());
+        reader->DetectNumericColumnsOn();
+        reader->SetFieldDelimiterCharacters(" ");
+        reader->Update();
+        
+        vtkTable* table = reader->GetOutput();
+        */
+
+        vtkNew<vtkUnsignedCharArray> colors;
+        colors->SetName("colors");
+        colors->SetNumberOfComponents(3);
+
+        
+        std::array<unsigned char, 3> color = { 255, 0, 0 };
+        std::array<unsigned char, 3> color1 = { 0, 255, 0 };
+        std::array<unsigned char, 3> color2 = { 0, 0, 255 };
+        std::array<unsigned char, 3> color3 = { 255, 255, 0 };
+        std::array<unsigned char, 3> color4 = { 255, 0, 255 };
+
+        for (vtkIdType i = 0; i < pointCount / 5; i++)
+        {
+            colors->InsertNextTypedTuple(color.data());
+            colors->InsertNextTypedTuple(color1.data());
+            colors->InsertNextTypedTuple(color2.data());
+            colors->InsertNextTypedTuple(color3.data());
+            colors->InsertNextTypedTuple(color4.data());
+        }
+        return colors;
     }
 
     void render(vtkActor* actor) {
         vtkNew<vtkRenderer> renderer;
         renderer->AddActor(actor);
 
-        renderer->SetBackground(colors->GetColor3d("LightYellow").GetData());
+        renderer->SetBackground(namedColors->GetColor3d("LightYellow").GetData());
         // Zoom in a little by accessing the camera and invoking its "Zoom" method.
         renderer->ResetCamera();
         renderer->GetActiveCamera()->Zoom(1.5);
@@ -86,6 +122,9 @@ int main() {
     vtkNew<vtkPolyData> polyData;
     polyData->SetPoints(points);
 
+    auto colors = loadColors(0, points->GetNumberOfPoints());
+    polyData->GetPointData()->SetScalars(colors);
+
     vtkNew<vtkGlyph3DMapper> glyph3D;
     glyph3D->SetInputData(polyData);
     glyph3D->SetSourceConnection(sphere->GetOutputPort());
@@ -94,7 +133,7 @@ int main() {
     vtkNew<vtkActor> actor;
     actor->SetMapper(glyph3D);
     actor->GetProperty()->SetPointSize(30);
-    actor->GetProperty()->SetColor(colors->GetColor3d("Tomato").GetData());
+    actor->GetProperty()->SetColor(namedColors->GetColor3d("Tomato").GetData());
 
     render(actor);
 
