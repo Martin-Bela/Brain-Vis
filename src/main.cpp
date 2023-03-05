6b80b53ac60f72ba0a58c9dccf2ff4d3a5ba8cc3
Martin Beľa <xbela1@fi.muni.cz>
2023-03-05 17:48:14 +0100

    Add coloring of groups of neuron

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,6 @@
 #include <vtkRenderer.h>
 #include <vtkTable.h>
 
-#include <array>
-#include <filesystem>
-
 #include <vtkDelimitedTextReader.h>
 #include <vtkVertexGlyphFilter.h>
 #include <vtkGlyph3D.h>
@@ -21,6 +18,14 @@
 #include <vtkSliderWidget.h>
 #include <vtkWidgetEvent.h>
 #include <vtkInteractorStyleTrackballCamera.h>
+
+#include <array>
+#include <filesystem>
+#include <random>
+
+#include "neuronProperties.hpp"
+
+std::mt19937 rand_gen(time(0));
 
 namespace { //anonymous namespace
 
@@ -48,38 +53,99 @@ namespace { //anonymous namespace
         return positions;
     }
 
-    vtkNew<vtkUnsignedCharArray> loadColors(uint64_t timestep, uint64_t pointCount) {
-        /*vtkNew<vtkDelimitedTextReader> reader;
-        auto path = (dataFolder / "monitors/0_").string();
-        path += std::to_string(timestep) + ".csv";
-        
-        reader->SetFileName(path.data());
-        reader->DetectNumericColumnsOn();
-        reader->SetFieldDelimiterCharacters(" ");
-        reader->Update();
-        
-        vtkTable* table = reader->GetOutput();
-        */
+    // from https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
+    std::array<float,3> hslToRgb(std::array<float, 3> hsl) {
+
+        auto hueToRgb = [](float p, float q, float t) {
+            if (t < 0.f)
+                t += 1.f;
+            if (t > 1.f)
+                t -= 1.f;
+            if (t < 1.f / 6.f)
+                return p + (q - p) * 6.f * t;
+            if (t < 1.f / 2.f)
+                return q;
+            if (t < 2.f / 3.f)
+                return p + (q - p) * (2.f / 3.f - t) * 6.f;
+            return p;
+        };
+
+        auto [h, s, l] = hsl;
+        float r, g, b;
+
+        if (s == 0.f) {
+            r = g = b = l; // achromatic
+        }
+        else {
+            float q = l < 0.5f ? l * (1 + s) : l + s - l * s;
+            float p = 2 * l - q;
+            r = hueToRgb(p, q, h + 1.f / 3.f);
+            g = hueToRgb(p, q, h);
+            b = hueToRgb(p, q, h - 1.f / 3.f);
+        }
+        return { r, g, b };
+    }
+
+    std::array<unsigned char, 3> generateNiceColor() {
+        std::uniform_real_distribution<float> dist(0, 1);
+        std::array<float, 3> hsl = { dist(rand_gen), 1.0, dist(rand_gen) / 2.f + 0.25f };
+        auto rgb = hslToRgb(hsl);
+        return { static_cast<unsigned char>(rgb[0] * 255),
+            static_cast<unsigned char>(rgb[1] * 255),
+            static_cast<unsigned char>(rgb[2] * 255) };
+    }
+
+    vtkNew<vtkUnsignedCharArray> loadColors(int timestep, int pointCount) {
+        auto path = (dataFolder / "monitors-bin/timestep").string() + std::to_string(timestep);
+        std::ifstream in(path);
 
         vtkNew<vtkUnsignedCharArray> colors;
         colors->SetName("colors");
         colors->SetNumberOfComponents(3);
 
-        
-        std::array<unsigned char, 3> color = { 255, 0, 0 };
-        std::array<unsigned char, 3> color1 = { 0, 255, 0 };
-        std::array<unsigned char, 3> color2 = { 0, 0, 255 };
-        std::array<unsigned char, 3> color3 = { 255, 255, 0 };
-        std::array<unsigned char, 3> color4 = { 255, 0, 255 };
+        for (int i = 0; i < pointCount; i++) {
+            NeuronProperties neuron{};
+            in.read(reinterpret_cast<char*>(&neuron), sizeof(NeuronProperties));
 
-        for (vtkIdType i = 0; i < pointCount / 5; i++)
-        {
-            colors->InsertNextTypedTuple(color.data());
-            colors->InsertNextTypedTuple(color1.data());
-            colors->InsertNextTypedTuple(color2.data());
-            colors->InsertNextTypedTuple(color3.data());
-            colors->InsertNextTypedTuple(color4.data());
+            std::array<unsigned char, 3> color = { 255, 0, 0};
+            std::array<unsigned char, 3> color2 = { 0, 255, 0 };
+
+            colors->InsertNextTypedTuple(neuron.electricActivity > -62 ? color.data() : color2.data());
         }
+
+        if (!in.good()) {
+            std::cout << "Error reading colors!\n" << std::endl;
+        }
+
+        return colors;
+    }
+
+    vtkNew<vtkUnsignedCharArray> colorsFromPositions(vtkPoints& points) {
+        vtkNew<vtkUnsignedCharArray> colors;
+        colors->SetName("colors");
+        colors->SetNumberOfComponents(3);
+
+        auto manhattan_dist = [](double* x, float* y) {
+            auto dx = x[0] - y[0];
+            auto dy = x[1] - y[1];
+            auto dz = x[2] - y[2];
+
+            return abs(dx) + abs(dy) + abs(dz);
+        };
+        
+        std::array<float, 3> prev_point{};
+        auto color = generateNiceColor();
+
+        for (int i = 0; i < points.GetNumberOfPoints(); i++) {
+            if (manhattan_dist(points.GetPoint(i), prev_point.data()) > 0.5) {
+                auto point = points.GetPoint(i);
+                prev_point = { float(point[0]), float(point[1]), float(point[2]) };
+                color = generateNiceColor();
+            }
+
+            colors->InsertNextTypedTuple(color.data());
+        }
+
         return colors;
     }
 
@@ -132,7 +198,8 @@ int main() {
     vtkNew<vtkPolyData> polyData;
     polyData->SetPoints(points);
 
-    auto colors = loadColors(0, points->GetNumberOfPoints());
+    //auto colors = loadColors(80, points->GetNumberOfPoints());
+    auto colors = colorsFromPositions(*points);
     polyData->GetPointData()->SetScalars(colors);
 
     vtkNew<vtkGlyph3DMapper> glyph3D;
