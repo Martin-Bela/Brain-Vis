6836c1a61e69f8229c3735734e1579423b960135
Martin Beľa <xbela1@fi.muni.cz>
2023-03-05 17:47:15 +0100

    Fix neuron properties preprocessing

--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -26,12 +26,15 @@ int main() {
     auto input_path = (dataFolder / "monitors" / "0_").string();
     auto output_path = (dataFolder / "monitors-bin" / "timestep").string();
 
-    auto start = steady_clock::now();
-    constexpr int step_count = 100 /*10'000*/;
+    auto global_start = steady_clock::now();
+    auto start = global_start;
+    constexpr int step_count = 50'000;
     for (int in_outer = 0; in_outer < step_count; in_outer += 50) {
-        auto end = steady_clock::now();
-        std::cout << in_outer << " files " << duration_cast<duration<double>>(end - start) << "\n";
-        start = end;
+        if (in_outer % 500 == 0) {
+            auto end = steady_clock::now();
+            std::cout << in_outer * 100 / 50'000 << "% " << duration_cast<duration<double>>(end - start) << "\n";
+            start = end;
+        }
 
         std::array<std::ifstream, 50> in_files;
         for (int in = 0; in < 50; in++) {
@@ -39,13 +42,22 @@ int main() {
             in_files[in].open(path);
         }
 
-        for (int out = 0; out < 50'000; out++) {
+        for (int out = 0; out < 100/*10'000*/; out++) {
             std::ofstream out_file(output_path + std::to_string(out), std::ios_base::binary | std::ios_base::app);
             for (int in = 0; in < 50; in++) {
                 auto neuron = NeuronProperties::Parse(in_files[in]);
                 out_file.write(reinterpret_cast<const char*>(&neuron), sizeof(neuron));
             }
+            if (!out_file.good()) {
+                std::cout << "Output file error\n" << std::endl;
+            }
+        }
+
+        for (auto& file : in_files) {
+            if (!file.good()) {
+                std::cout << "Input file error\n" << std::endl;
+            }
         }
     }
-    std::cout << step_count << " files " << duration_cast<duration<double>>(steady_clock::now() - start) << "\n";
+    std::cout << "Duration: " << duration_cast<duration<double>>(steady_clock::now() - global_start) << "\n";
 }
